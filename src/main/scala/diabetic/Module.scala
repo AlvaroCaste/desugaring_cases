ffda3ecb7c9b8ae2f89f4e35e2c40e21ef2aee82
Álvaro Castellanos <alvaro.castellanos.lopez@gmail.com>
2016-07-03 17:00:29 +0200

    Make Module a product type

--- src/main/scala/diabetic/Module.scala
+++ src/main/scala/diabetic/Module.scala
@@ -1,5 +1,13 @@
 package diabetic
 
-object Module extends Serializable {
+object Module extends Serializable with Product {
   override def toString = "Module"
+
+  override def productElement(n: Int): Any = ???
+
+  override def productArity: Int = 0
+
+  override def productPrefix: String = this.toString
+
+  override def canEqual(that: Any): Boolean = ???
 }
--- src/test/scala/diabetic/ModuleTest.scala
+++ src/test/scala/diabetic/ModuleTest.scala
@@ -12,9 +12,9 @@ class ModuleTest extends FlatSpec with ShouldMatchers {
     Module shouldBe a [Serializable]
   }
 
-//  it should "be a product" in {
-//    Module.productPrefix shouldBe "Module"
-//    Module.productArity shouldBe 0
-//    Module.productIterator shouldBe 'empty
-//  }
+  it should "be a product" in {
+    Module.productPrefix shouldBe "Module"
+    Module.productArity shouldBe 0
+    Module.productIterator shouldBe 'empty
+  }
 }
