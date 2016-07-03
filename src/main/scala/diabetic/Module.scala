85f07c0c0f4db24e48aa370db046a7b8c218b489
Álvaro Castellanos <alvaro.castellanos.lopez@gmail.com>
2016-07-03 16:55:05 +0200

    Add a nice toString method to Module

--- src/main/scala/diabetic/Module.scala
+++ src/main/scala/diabetic/Module.scala
@@ -1,3 +1,5 @@
 package diabetic
 
-object Module
+object Module {
+  override def toString = "Module"
+}
--- src/test/scala/diabetic/ModuleTest.scala
+++ src/test/scala/diabetic/ModuleTest.scala
@@ -4,10 +4,10 @@ import org.scalatest.{FlatSpec, ShouldMatchers}
 
 class ModuleTest extends FlatSpec with ShouldMatchers {
 
-//  "Module" should "have a nice string conversion" in {
-//    Module.toString shouldBe "Module"
-//  }
-//
+  "Module" should "have a nice string conversion" in {
+    Module.toString shouldBe "Module"
+  }
+
 //  it should "be serializable" in {
 //    Module shouldBe a [Serializable]
 //  }
